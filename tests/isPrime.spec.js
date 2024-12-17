aef13aabb78995e7922a9d9a1d95c33e74c2613c
Abu Hurayra <hurayraiit@gmail.com>
2024-12-17 14:34:24 +0600

    separated test cases

--- tests/isPrime.spec.js
+++ tests/isPrime.spec.js
@@ -4,19 +4,46 @@ import measurePerformance from "../utils/performanceChecker";
 import isPrime from "../challenges/isPrime";
 
 test.describe("isPrime", () => {
-  test("should return true for prime numbers", async () => {
+  test("should return true for 2", async () => {
     expect.soft(isPrime(2)).toBe(true);
+  });
+
+  test("should return true for 3", async () => {
     expect.soft(isPrime(3)).toBe(true);
+  });
+
+  test("should return true for 5", async () => {
     expect.soft(isPrime(5)).toBe(true);
+  });
+
+  test("should return true for 7", async () => {
     expect.soft(isPrime(7)).toBe(true);
+  });
+
+  test("should return true for 29", async () => {
     expect.soft(isPrime(29)).toBe(true);
   });
 
-  test("should return false for non-prime numbers", async () => {
+  test("should return false for 0", async () => {
     expect.soft(isPrime(0)).toBe(false);
+  });
+
+  test("should return false for 1", async () => {
     expect.soft(isPrime(1)).toBe(false);
     expect.soft(isPrime(4)).toBe(false);
     expect.soft(isPrime(6)).toBe(false);
+    expect.soft(isPrime(9)).toBe(false);
+  });
+
+  test("should return false for 4", async () => {
+    expect.soft(isPrime(4)).toBe(false);
+  });
+
+  test("should return false for 6", async () => {
+    expect.soft(isPrime(6)).toBe(false);
+  });
+
+  test("should return false for 9", async () => {
     expect.soft(isPrime(9)).toBe(false);
   });
 });
@@ -27,8 +54,6 @@ test.describe("Multiple Prime Time Efficiency Tests", () => {
   for (const num of largeNumbers) {
     test(`Testing isPrime(${num})`, async () => {
       const { executionTime, memoryUsed, result } = measurePerformance(isPrime, num);
-    //   expect(result).toBe(true);
-      // Check performance thresholds
       expect.soft(executionTime).toBeLessThan(100); // Less than 100ms
       expect.soft(memoryUsed.rss).toBeLessThan(100 * 1024); // Less than 100MB
     });
